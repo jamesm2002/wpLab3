cb067dd982b4f8af105b94e1179ac3792be1ce00
jamesm2002 <91893668+jamesm2002@users.noreply.github.com>
2021-11-18 15:39:24 +0000

    eh

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+//creating app
+const app = express();
+//send an HTTP response when receiving HTTP GET /
+app.get("/", (req, res) => {
+  res.send("Hello, you're server is up and running");
+});
+//make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`Cart app listening at http://localhost:${port}`);
+});
+//send the index.html when receiving HTTP GET /
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html", { root: __dirname });
+});
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
+});
